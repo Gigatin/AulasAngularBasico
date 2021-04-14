6c1422f74ad7c9409608379b7ae5279dcf9f7986
Gabriel Caetano <gabrielcaetano41@hotmail.com>
2021-04-14 15:13:31 -0300

    AulasIniciaisAngular

---
+++ DesenvolvimentoAngular/hello-typescript/main.js
@@ -0,0 +1,16 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'asdas';
+n1 = 4;
---
+++ DesenvolvimentoAngular/hello-typescript/main.ts
@@ -0,0 +1,19 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y) {
+return x + y;
+
+}
+
+
+let num = 2;
+var PI = 3.14;
+
+class Matematica {
+  
+	soma(x, y){
+      return x + y;
+      }
+}
+var n1 = 'asdas';
+n1 = 4;
